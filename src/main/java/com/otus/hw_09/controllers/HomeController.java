e8b3e221719f15e7137022960e35ea8438b2e709
Andrei Bogomja <bogomja@gmail.com>
2019-03-16 21:52:44 +0000

    Adds rest api for home controller

--- src/main/java/com/otus/hw_09/controllers/HomeController.java
+++ src/main/java/com/otus/hw_09/controllers/HomeController.java
@@ -1,21 +1,13 @@
 package com.otus.hw_09.controllers;
 
-import com.otus.hw_09.library.LibraryService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequiredArgsConstructor
 public class HomeController {
 
-    private final LibraryService libraryService;
-
     @GetMapping("/home")
-    public String index(final Model model) {
-        model.addAttribute("booksTotal", libraryService.getBooksCount());
-        model.addAttribute("authorsTotal", libraryService.getAuthorsCount());
+    public String index() {
         return "index";
     }
 
---
+++ src/main/java/com/otus/hw_09/controllers/HomeRESTController.java
@@ -0,0 +1,31 @@
+package com.otus.hw_09.controllers;
+
+import com.otus.hw_09.repositories.AuthorRepository;
+import com.otus.hw_09.repositories.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class HomeRESTController {
+
+    private final BookRepository bookRepo;
+    private final AuthorRepository authorRepo;
+
+    @GetMapping(path = "/library/totals", produces = {"application/json"})
+    public ResponseEntity<Map> getLibraryTotals() {
+        final long booksCount = bookRepo.count();
+        final long authorsCount = authorRepo.count();
+        final Map<String, Long> totals = new HashMap<>();
+        totals.put("booksCount", booksCount);
+        totals.put("authorsCount", authorsCount);
+        return new  ResponseEntity<>(totals, HttpStatus.OK);
+    }
+
+}
